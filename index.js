d01697cee24b3e511b5260886489de4eae108efd
ROMANOMATMAX <romanomatias99@gmail.com>
2021-06-07 23:09:54 -0300

    primer commit / maquetado de la seccion principal

---
+++ index.js
@@ -0,0 +1,18 @@
+const buttonLeft = document.getElementById("button-carousel-left");
+const carouselActive = document.getElementsByClassName("carousel-item active")[0];
+const itemUnoCarousel = document.getElementById("item-1-carousel");
+
+buttonLeft.addEventListener('click', () => {
+    console.log("Previus");
+})
+
+const buttonRight = document.getElementById("button-carousel-right");
+
+buttonRight.addEventListener('click', () => {
+    console.log("Next");
+})
+
+// window.onload = function() {
+//     itemUnoCarousel.classList.add("carousel-animation")
+// };
+
