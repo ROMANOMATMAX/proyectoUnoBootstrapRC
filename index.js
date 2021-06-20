1c6d3f95943a79f6b6f7307ae2189391b700bf94
Matias <romanomatias99@gmail.com>
2021-06-20 03:54:09 -0300

    arreglando los botones para deslizar el carrousel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const buttonRight = document.getElementById("button-carousel-right");
 const buttonLeft = document.getElementById("button-carousel-left");
 const carouselActive = document.getElementsByClassName("carousel-item active")[0];
 const itemUnoCarousel = document.getElementById("item-1-carousel");
@@ -13,33 +14,77 @@ const tematicaTres = document.getElementById("tematicaTres");
 const amountOfImgInCarrousel =  mainCarrouselContainer.children.length;
 console.log("Children amount", amountOfImgInCarrousel);
 
+if (window.matchMedia("(max-width: 768px)").matches) {
+var myCarousel = document.querySelector('#carouselExampleControls')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false,
+})
+}
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var myCarousel = document.querySelector('#carouselExampleControls')
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: false,
+    touch: false
+  })
+  }
+  
+
 buttonLeft.addEventListener('click', () => {
-    console.log("Previus");
+  console.log("move to right");
+  console.log("Next");
+  console.log(miniCarrouselItems);
+  let arry = [...miniCarrouselItems]
+  let arryImages = [...miniCarrouselImages] 
+  console.log(arry);
+  // let currentPositionCardHover = -1;
+  for(let i= 0; i<arry.length; i++){
+    if(arry[i].className ==="carrouselItem cardHover"){
+      arry[i].className = "carrouselItem"
+      arryImages[i].className = "img-item-carrousel w-100"
+      console.log(arry.children)
+      if(i !== 0){
+        currentPositionCardHover = i;
+      }else{
+        currentPositionCardHover = arry.length;
+      }
+      console.log(currentPositionCardHover);
+    }
+  }
+  arry[currentPositionCardHover-1].className = "carrouselItem cardHover"
+  arryImages[currentPositionCardHover-1].className = "img-item-carrousel w-100 opacity-lower"
+  console.log(currentPositionCardHover);
 })
 
-const buttonRight = document.getElementById("button-carousel-right");
+
 let iteradorMiniCarrousel = 0;
 let currentPositionCardHover = -1;
-// buttonRight.addEventListener('click', () => {
-//     console.log("Next");
-//     console.log(miniCarrouselItems);
-//     let arry = [...miniCarrouselItems] 
-//     console.log(arry);
-//     // let currentPositionCardHover = -1;
-//     for(let i= 0; i<arry.length; i++){
-//       if(arry[i].className ==="carrouselItem cardHover"){
-//         arry[i].className = "carrouselItem"
-//         if(i !== arry.length -1){
-//           currentPositionCardHover = i;
-//         }else{
-//           currentPositionCardHover = -1;
-//         }
-//         console.log(currentPositionCardHover);
-//       }
-//     }
-//     arry[currentPositionCardHover+1].className = "carrouselItem cardHover"
-//     console.log(currentPositionCardHover);
-// })
+buttonRight.addEventListener('click', () => {
+  console.log("move to left");
+      console.log("Next");
+      console.log(miniCarrouselItems);
+      let arry = [...miniCarrouselItems] 
+      let arryImages = [...miniCarrouselImages] 
+      console.log(arry);
+      console.log(arryImages);
+      // let currentPositionCardHover = -1;
+      for(let i= 0; i<arry.length; i++){
+        if(arry[i].className ==="carrouselItem cardHover"){
+          arry[i].className = "carrouselItem"
+          arryImages[i].className = "img-item-carrousel w-100"
+          console.log(arry.children)
+          if(i !== arry.length -1){
+            currentPositionCardHover = i;
+          }else{
+            currentPositionCardHover = -1;
+          }
+          console.log(currentPositionCardHover);
+        }
+      }
+      arry[currentPositionCardHover+1].className = "carrouselItem cardHover"
+      arryImages[currentPositionCardHover+1].className = "img-item-carrousel w-100 opacity-lower"
+      console.log(currentPositionCardHover);
+})
 
 // window.onload = function() {
 //     itemUnoCarousel.classList.add("carousel-animation")
@@ -48,10 +93,7 @@ let currentPositionCardHover = -1;
 //     console.log("Estas en pantalla pequeña");
 //     buttonLeft.click();
 // }
-var myCarousel = document.querySelector('#carouselExampleControls')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: false,
-})
+
 
 //JS para la animación de cards apenas aparecen en el viewport
 const listOfCards = document.getElementsByClassName("card");
@@ -133,10 +175,14 @@ function move(e) {
     x0 = null
   }
 };
-mainCarrouselContainer.addEventListener('mousedown', lock, false);
-mainCarrouselContainer.addEventListener('touchstart', lock, false);
-mainCarrouselContainer.addEventListener('mouseup', move, false);
-mainCarrouselContainer.addEventListener('touchend', move, false);
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+  mainCarrouselContainer.addEventListener('touchstart', lock, false);
+  mainCarrouselContainer.addEventListener('touchend', move, false);
+  mainCarrouselContainer.addEventListener('mousedown', lock, false);
+  mainCarrouselContainer.addEventListener('mouseup', move, false);
+}
+
 
 linkUno.addEventListener('click', () => {
   console.log("Click a Link 2");
